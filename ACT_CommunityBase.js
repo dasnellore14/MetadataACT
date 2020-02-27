3aaa1fca9c79c715c918b728e52441ff68c91fc0
dasnellore14 <47324180+dasnellore14@users.noreply.github.com>
2020-02-27 22:28:51 +0530

    Create ACT_CommunityBase.js

---
+++ ACT_CommunityBase.js
@@ -0,0 +1,8 @@
+/**
+ * Created by adamsm on 1/25/2019.
+ */
+({
+    initCommunityBase : function(component, event, helper) {
+        helper.showConsole(component, 'Community Base Initialized.');
+    },
+})
